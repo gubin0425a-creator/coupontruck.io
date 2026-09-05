261217d7f48721644f9b69e7399b22839565d45d
gubin0425a-creator <gubin0425a-creator@users.noreply.github.com>
2026-09-05 13:57:32 +0900

    feat: integrate official GamsGo affiliate link (aTqwg)

--- main.js
+++ main.js
@@ -1219,7 +1219,7 @@ function getFallbackData() {
           "name": "겜스고 (GamsGo)",
           "code": "GAMSGO5",
           "desc": "유튜브 프리미엄, 넷플릭스 70% 계정공유 5% 추가할인 코드 (월 3천원대)",
-          "url": "https://www.gamsgo.com",
+          "url": "https://www.gamsgo.com/partner/aTqwg",
           "expires": "2026-12-31",
           "is_active": true,
           "badge": "인기 1위"
